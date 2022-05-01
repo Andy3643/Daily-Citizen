14c2e51a86e087d4d88ef384cb4daad9ad0d7878
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-01 06:23:27 +0300

    feat:add app configuration in main file

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+#building brains
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+#creating app configurations
+def create_app (config_name):
+    app = Flask (__name__)
+    app.config.from_object(config_options[config_name])
+    bootstrap.init_app(app)
+    
+    #register blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
 export NEWS_API_KEY = "0d17d16443b24e18b582d11709286dfb"
+python3 manage.py server
