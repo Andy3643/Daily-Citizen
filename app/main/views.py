3ce3edc9ebd0303c216d296e83eee9851d10a570
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-02 11:29:23 +0300

    file:add main.html file to compile list from source object

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,11 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
-#from ..newsrequests import 
+from ..newsrequests import  get_article,get_sources,search_url
+
+@main.route('/')
+def index ():
+    articles = get_article('source_id')
+    sources = get_sources ()
+    # place search functionality below and use it to make requests
+    #if else ststemnt
+    return render_template('index.html',articles=articles,sources=sources)
